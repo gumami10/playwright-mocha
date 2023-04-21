e2b59bfcb2e0b480335f43be7c7a01e4074c3b69
Eric Groppe <emmgp@hotmail.com>
2023-04-21 16:01:00 -0300

    test: mocha implementation

---
+++ api-tests/request.spec.ts
@@ -0,0 +1,13 @@
+import supertest from "supertest";
+import { url } from "./utils";
+import {expect} from "chai"
+const request = supertest(url);
+
+
+describe('people', async () => {
+    it('get people async', async () => {
+        let res = await request.get('people/1')
+
+        expect(res.body.name).to.eql("Luke Skywalker")
+    })
+})
